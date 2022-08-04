1beaf922f84c1f99ec75d3f5a8dfbb5f47685dcc
Fever <SodaZhang1217@qq.com>
2022-08-04 20:08:23 +0800

    second commit

--- src/main/java/com/Fever/GitTest.java
+++ src/main/java/com/Fever/GitTest.java
@@ -3,5 +3,6 @@ package com.Fever;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello man");
+        System.out.println("hello man2");
     }
 }
